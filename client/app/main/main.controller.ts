e1a9db0d39b601e7cc522842354901374f269a3b
camendoz <cams.0093@gmail.com>
2016-06-19 17:15:41 -0500

    store original results without rounding

--- client/app/main/main.controller.ts
+++ client/app/main/main.controller.ts
@@ -13,6 +13,12 @@ class MainController {
     $scope.lq = "NaN";
     $scope.wq = "NaN";
     $scope.p = "NaN";
+    $scope.op0 = "";
+    $scope.ol = "";
+    $scope.ow = "";
+    $scope.olq = "";
+    $scope.owq = "";
+    $scope.op = "";
     $scope.chanel = "";
     $scope.lambda = "";
     $scope.tasa = "";
@@ -27,6 +33,13 @@ class MainController {
         $scope.calculateP();
       }
     };
+    $scope.rFact = function(num){
+      var rval = 1;
+      for (var i = 2; i <= num; i++) {
+        rval = rval * i;
+      }
+      return rval;
+    };
     $scope.calculatePO = function(){
       var result = 0;
       var fact = 0;
@@ -38,14 +51,8 @@ class MainController {
       result = result + (1 / fact) * Math.pow($scope.lambda / $scope.tasa, $scope.chanel) *
         (($scope.chanel * $scope.tasa) / ($scope.chanel * $scope.tasa - $scope.lambda));
       result = 1 / result;
+      $scope.op0 = result;
       $scope.p0 = Math.round(result * 1000) / 1000;
-    };
-    $scope.rFact = function(num){
-      var rval = 1;
-      for (var i = 2; i <= num; i++) {
-        rval = rval * i;
-      }
-      return rval;
     };
     $scope.calculateL = function(){
       var result = 0;
@@ -54,26 +61,31 @@ class MainController {
       result = result + $scope.lambda * $scope.tasa * Math.pow($scope.lambda / $scope.tasa, $scope.chanel) * $scope.p0;
       result = result / (fact * Math.pow($scope.chanel * $scope.tasa - $scope.lambda, 2));
       result = result + ($scope.lambda / $scope.tasa);
+      $scope.ol = result;
       $scope.l = Math.round(result * 1000) / 1000;
     };
     $scope.calculateW = function(){
       var result = 0;
       result = $scope.l / $scope.lambda;
+      $scope.ow = result;
       $scope.w = Math.round(result * 1000) / 1000;
     };
     $scope.calculateLQ = function(){
       var result = 0;
       result = $scope.l - $scope.lambda / $scope.tasa;
+      $scope.olq = result;
       $scope.lq = Math.round(result * 1000) / 1000;
     };
     $scope.calculateWQ = function(){
       var result = 0;
       result = $scope.lq / $scope.lambda;
+      $scope.owq = result;
       $scope.wq = Math.round(result * 1000) / 1000;
     };
     $scope.calculateP = function(){
       var result = 0;
       result = $scope.lambda / ($scope.chanel * $scope.tasa);
+      $scope.op = result;
       $scope.p = Math.round(result * 1000) / 1000;
     };
   }
