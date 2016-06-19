cff31d972eeb034ff5fbafbdac995ddfd241acd9
camendoz <cams.0093@gmail.com>
2016-06-19 14:05:45 -0500

    modify footer, add queue variables to the view and the controller

--- client/app/main/main.controller.ts
+++ client/app/main/main.controller.ts
@@ -7,14 +7,18 @@ class MainController {
   constructor($http, $scope) {
     this.$http = $http;
     this.awesomeThings = [];
+    $scope.p0="1";
+    $scope.l="2";
+    $scope.w="3";
+    $scope.lq="4";
+    $scope.wq="5";
+    $scope.p="6";
     // function to submit the form after all validation has occurred
     $scope.submitForm = function(isValid) {
-
       // check to make sure the form is completely valid
       if (isValid) {
         alert('our form is amazing');
       }
-
     };
   }
 
