a9f354ed1d86b652e4458d5bb36d861b00445032
camendoz <cams.0093@gmail.com>
2016-06-19 00:38:30 -0500

    add validations in the input fields

--- client/app/main/main.controller.ts
+++ client/app/main/main.controller.ts
@@ -4,9 +4,18 @@
 
 class MainController {
 
-  constructor($http) {
+  constructor($http, $scope) {
     this.$http = $http;
     this.awesomeThings = [];
+    // function to submit the form after all validation has occurred
+    $scope.submitForm = function(isValid) {
+
+      // check to make sure the form is completely valid
+      if (isValid) {
+        alert('our form is amazing');
+      }
+
+    };
   }
 
   $onInit() {
